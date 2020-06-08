afe5ac1fb1a95c4563b5b1c0814fb5a5b569a1b9
Pandu Abbiyuarsyah <abbiyuarsyah@gmail.com>
2020-06-08 17:52:17 +0700

    Initial Commit

---
+++ medicines-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  medicines-app
+//
+//  Created by Pandu Abbiyuarsyah on 08/06/20.
+//  Copyright © 2020 Pandu Abbiyuarsyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
